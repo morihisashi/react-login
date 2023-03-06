cb6ee760a5f045abc6c1edab56d24b5b9e511cb7
morihisashi <hisashi.mori@humanage.co.jp>
2023-03-06 10:43:24 +0900

    Sign In画面作成

---
+++ login-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import { Homepage } from './pages/Homepage';
+import './App.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ login-app/src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+export function Homepage() {
+    return (
+        <div className={'div'}>
+            <h2>Sign In</h2>
+            <p>e-mail</p>
+            <input></input>
+            <p>パスワード</p>
+            <input></input>
+            <p>パスワードを忘れた場合はこちら</p>
+            <p>新規登録はこちら</p>
+        </div>
+    );
+}
